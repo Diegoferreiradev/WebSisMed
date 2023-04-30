285a20be20d5afabcb140a9002e83e9cbcb9f42f
Diegoferreiradev <diegoferreiradev@gmail.com>
2023-04-30 17:56:57 -0300

    Adição do ViewModel de Médicos

---
+++ ViewModels/Medicos/AdicionarMedicoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebSisMed.ViewModels.Medicos
+{
+    public class AdicionarMedicoViewModel
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string CRM { get; set; } = string.Empty;
+    }
+}
---
+++ ViewModels/Medicos/ListarMedicoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSisMed.ViewModels.Medicos
+{
+    public class ListarMedicoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string CRM { get; set; } = string.Empty;
+    }
+}
