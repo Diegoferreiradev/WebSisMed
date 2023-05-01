af4228d955323eafe73fa42cf53dfc6cd90ea102
Diegoferreiradev <diegoferreiradev@gmail.com>
2023-05-01 02:22:57 -0300

    Adição dos ViewModels da Consulta

---
+++ ViewModels/Consultas/AdicionarConsultaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebSisMed.Models.Enums;
+
+namespace WebSisMed.ViewModels.Consultas
+{
+    public class AdicionarConsultaViewModel
+    {
+        [DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+        public TipoConsulta Tipo { get; set; }
+
+        [Display(Name = "Paciente")]
+        public int IdPaciente { get; set; }
+
+        [Display(Name = "Médico")]
+        public int IdMedico { get; set; }
+    }
+}
---
+++ ViewModels/Consultas/EditarConsultaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebSisMed.Models.Enums;
+
+namespace WebSisMed.ViewModels.Consultas
+{
+    public class EditarConsultaViewModel
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+        public TipoConsulta Tipo { get; set; }
+
+        [Display(Name = "Paciente")]
+        public int IdPaciente { get; set; }
+
+        [Display(Name = "Nome do Paciente")]
+        public string NomePaciente { get; set; } = string.Empty;
+
+        [Display(Name = "Médico")]
+        public int IdMedico { get; set; }
+    }
+}
